472547edf31950f007ba84ccc4e483f5996f0c55
twins1909 <twins1909@naver.com>
2026-05-13 12:28:28 +0900

    [Passed] 4가지 관계연산자 (47ms, 16MB)

---
+++ trail1/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,25 @@
+a, b = map(int, input().split())
+
+
+if a >= b:
+    print("1")
+else:
+    print("0")
+
+    
+if a > b:
+    print("1")
+else:
+    print("0")
+
+    
+if a <= b:
+    print("1")
+else:
+    print("0")
+
+    
+if a < b:
+    print("1")
+else:
+    print("0")
