07ec7fcacc495995a6e65f982ca2a70af93ed10c
twins1909 <twins1909@naver.com>
2026-05-14 19:55:10 +0900

    [Passed] 좀 더 어려운 수학 점수 (44ms, 16MB)

---
+++ trail1/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,13 @@
+a_math, a_eng = map(int, input().split())
+b_math, b_eng = map(int, input().split())
+
+if a_math > b_math:
+    print("A")
+elif b_math > a_math:
+    print("B")
+
+else:
+    if a_eng > b_eng:
+        print("A")
+    else:
+        print("B")
