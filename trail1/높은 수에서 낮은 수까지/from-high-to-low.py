4e3cc4fa921fbadae2e2a76d8038e3b140ded914
twins1909 <twins1909@naver.com>
2026-07-14 15:38:29 +0900

    [Passed] 높은 수에서 낮은 수까지 (53ms, 16MB)

---
+++ trail1/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+
+if a > b:
+    for i in range(a, b-1, -1):
+        print(i, end=" ")
+
+elif a < b:
+    for i in range(b, a-1, -1):
+        print(i, end=" ")
+
+else:
+    print(a)
