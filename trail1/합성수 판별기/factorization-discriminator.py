9da73d3f58170395f406fc3a3f9bec34645e9644
twins1909 <twins1909@naver.com>
2026-09-15 16:34:42 +0900

    [Passed] 합성수 판별기 (52ms, 16MB)

---
+++ trail1/합성수 판별기/factorization-discriminator.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+satisfied = False
+
+for i in range(2, n):
+
+    if n % i == 0:
+        satisfied = True
+        break
+
+if satisfied:
+    print("C")
+
+else:
+    print("N")
+
