e8ad1d5be5e59f7fb6e16ca5f761da48ddc010b9
twins1909 <twins1909@naver.com>
2026-09-15 16:43:03 +0900

    [Passed] 공약수의 유무 (60ms, 16MB)

---
+++ trail1/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+
+satisfied = False
+
+for i in range(a, b+1):
+
+    if (1920 % i == 0) and (2880 % i == 0):
+
+        satisfied = True
+        break
+
+if satisfied:
+    print("1")
+
+else:
+    print("0")
