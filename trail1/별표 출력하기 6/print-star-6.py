e4369d7c5a023ea4a5c59b185735548cfe1a4a19
twins1909 <twins1909@naver.com>
2026-09-22 16:33:23 +0900

    [Passed] 별표 출력하기 6 (50ms, 16MB)

---
+++ trail1/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+
+for i  in range(n):
+
+    for j in range(i):
+        print("  ", end="")
+
+
+    for j in range(2*(n-i)-1):
+
+        print("*", end=" ")
+
+    print()
+
+for i in range(n-1):
+
+    for j in range(n-2-i):
+        print("  ", end="")
+
+    for j in range(2*(i+1)+1):
+
+        print("*", end=" ")
+
+    print()
